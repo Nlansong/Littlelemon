ab3f2af267db2cb5c8551b910cd97d754587304e
[Nlansong] <[brightwumbei2018@gmail.com]>
2023-05-13 05:26:10 -0700

    added models

--- Littlep/settings.py
+++ Littlep/settings.py
@@ -76,8 +76,15 @@ WSGI_APPLICATION = 'Littlep.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'restaurant',
+        'USER': 'root',
+        'PASSWORD': '2018@gmail.com',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
+        'OPTIONS':{
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"
+        }
     }
 }
 
